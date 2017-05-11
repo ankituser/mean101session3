cfe867ebede9993af29a5ee86e63356dd8d5400b
ankituser <apptesting2012@gmail.com>
2017-05-11 08:00:53 +0000

    commit1

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mittens');
-
+app.use(express.static('public')); 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
+app.get('/meows',function(req,res,next){
+  Meow.find({},function(err,meows){
+    return res.json(meows);
+  });
+});
+
+//test12
+
 
